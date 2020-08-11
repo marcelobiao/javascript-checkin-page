8ec28110ae461893d4995ee1a41901c6f38dd2ee
marcelobiao <marcelobiao2@gmail.com>
2020-08-11 19:51:19 -0300

    Refatorando rota para redirecionar o usuário após o check-in

--- src/pages/CheckIn/index.js
+++ src/pages/CheckIn/index.js
@@ -1,10 +1,5 @@
 import React, {useState} from 'react';
-import {Link, useHistory} from 'react-router-dom';
-import {FiLogIn} from 'react-icons/fi';
-import { Redirect } from 'react-router-dom';
-import firebase from 'firebase';
-
-import api from '../../services/api';
+import axios from 'axios';
 
 import './styles.css';
 
@@ -13,23 +8,18 @@ import logoJSDay from '../../assets/596.png';
 
 export default function CheckIn(props){
     const[email, setEmail] = useState('');
-    const[error, setError] = useState('');
-    const history = useHistory();
 
     async function handleLogin(e){
         e.preventDefault();
         console.log('Teste' + process.env.teste);
-        try{
-            if(email == ''){
-                alert('Por favor preencha seu email no campo indicado');
-                return;
-            }
-            
-
-            //window.location.href = "https://www.youtube.com/watch?v=tvzgh9PhFf4";
-        }catch(err){
-            alert(err);
+        if(email == ''){
+            alert('Por favor preencha seu email no campo indicado');
+            return;
         }
+
+        //https://docs.google.com/forms/d/e/1FAIpQLSdzpg_SoA3AATvtktyve1C7JTM7VLZ9D97eG9JZUsJdROfQ7g/formResponse?entry.416908338=t@d.com
+        axios.post('https://docs.google.com/forms/d/e/1FAIpQLSdzpg_SoA3AATvtktyve1C7JTM7VLZ9D97eG9JZUsJdROfQ7g/formResponse?entry.416908338=' + email);
+        window.location.href = "https://www.youtube.com/watch?v=tvzgh9PhFf4";
     }
 
     return (
