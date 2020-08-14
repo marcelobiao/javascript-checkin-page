1df954ed52587b51cd4a59cf658cf2be3be447cd
marcelobiao <marcelobiao2@gmail.com>
2020-08-14 18:35:54 -0300

    Ajustando layout da aplicação

--- src/pages/CheckIn/index.js
+++ src/pages/CheckIn/index.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 import './styles.css';
 
 import logoGdg from '../../assets/250x106.png';
-import logoJSDay from '../../assets/596.png';
+//import logoJSDay from '../../assets/596.png';
+import logoJSDay from '../../assets/newlogo.png';
 
 export default function CheckIn(props){
     const[email, setEmail] = useState('');
@@ -19,7 +20,7 @@ export default function CheckIn(props){
 
         //https://docs.google.com/forms/d/e/1FAIpQLSdzpg_SoA3AATvtktyve1C7JTM7VLZ9D97eG9JZUsJdROfQ7g/formResponse?entry.416908338=t@d.com
         axios.post('https://docs.google.com/forms/d/e/1FAIpQLSdzpg_SoA3AATvtktyve1C7JTM7VLZ9D97eG9JZUsJdROfQ7g/formResponse?entry.416908338=' + email);
-        window.location.href = "https://www.youtube.com/watch?v=tvzgh9PhFf4";
+        window.location.href = "https://www.youtube.com/watch?v=LRRlOdKolRc&feature=youtu.be";
     }
 
     return (
